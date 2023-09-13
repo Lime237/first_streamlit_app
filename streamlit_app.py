5f03f156b6b18cbbec15b7db224dec3a930594ce
Lime237 <144887993+Lime237@users.noreply.github.com>
2023-09-13 10:22:42 +0200

    Update streamlit_app.py
    Add pandas and the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title ('My parents new healthy diner')
 
@@ -7,3 +8,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") # pull the List from the link
+streamlit.dataframe(my_fruit_list) # display the list in streamlit
