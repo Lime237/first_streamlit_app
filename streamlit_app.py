e8365842e646cef9abab1aa417415b66235e2645
Lime237 <144887993+Lime237@users.noreply.github.com>
2023-09-13 10:11:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ('My parents new healthy diner')
 
-streamlit.subtitle('Breakfast Menu')
+#streamlit.subtitle('Breakfast Menu')
 
 println('Omega 3 & Blueberry Oatmeal')
-println('Kale')
+
