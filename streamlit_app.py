68f51d5a49b6f6e9304c674d13da4579a1e7b926
Lime237 <144887993+Lime237@users.noreply.github.com>
2023-09-14 12:58:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
 
-#
+
+
 streamlit.title ('My parents new healthy diner')
 
 streamlit.header('🍌🥭Breakfast Menu')
@@ -30,3 +30,12 @@ streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
+
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
